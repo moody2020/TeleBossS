68d7b6a0d8792361c4d7fd485285c4a2a09cc837
moody2020 <mrmohammed98@mail.ru>
2016-08-19 10:43:18 +0300

    Update delete.lua

--- plugins/delete.lua
+++ plugins/delete.lua
@@ -12,29 +12,29 @@ local function history(extra, suc, result)
     delete_msg(result[i].id, ok_cb, false)
   end
   if tonumber(extra.con) == #result then
-    send_msg(extra.chatid, '"'..#result..'" من الرسائل تم تنظيفها 🍃', ok_cb, false)
+    send_msg(extra.chatid, '"'..#result..'" Of messages has been cleaned    ', ok_cb, false)
   else
-send_msg(extra.chatid, 'تم ☑️ تـنـظـيـف الـمـجـمـوعـه 🛡👍🏿 🍃', ok_cb, false)
+send_msg(extra.chatid, 'Group successfully been cleaned', ok_cb, false)
 end
 end
 local function mohammed(msg, matches)
-  if matches[1] == 'تنظيف' and is_sudo(msg) then
+  if matches[1] == 'clean' and is_sudo(msg) then
     if msg.to.type == 'channel' then
     if tonumber(matches[2]) > 100000 or tonumber(matches[2]) < 1 then
-        return "يمكنك وضع عدد100000 رسالة او اقل فقط"
-      end
+        return "You can put the message number 100000 or less just"
+         end
       get_history(msg.to.peer_id, matches[2] + 1 , history , {chatid = msg.to.peer_id, con = matches[2]})
     else
       return ""
     end
   else
-    return "🙈 لا تـلعـب بـكـيـفـك    ✋☝🏿️"
+    return "🙈 Developers Only📌"
   end
 end
 
 return {
     patterns = {
-        '^(تنظيف) (%d*)$'
+        '^(clean) (%d*)$'
     },
     run = mohammed
 }
